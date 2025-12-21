078673067ddcacb2487b4317402ee0549f79f3c2
zarafini <ira@proedge.co>
2025-12-21 11:23:50 +0530

    Add files via upload

---
+++ print.py
@@ -0,0 +1,17 @@
+#My name
+print("My name is Ira")
+
+#My hobbies
+print("My hobbies are playing The piano, riding horses,etc.")
+
+#Simple multiplication
+print("The product of 3*4 is:", 3*4)
+
+#Simple addition
+print("The  sum of 3+4 is:", 3+4)
+
+#Simple subtraction
+print("The result of 8 - 5 is:",8 - 5)
+
+#My age
+print("My age is 13")
