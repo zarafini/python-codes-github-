a85857161c627060955f73372b27c0df6bc7c926
zarafini <ira@proedge.co>
2025-12-22 10:28:57 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,21 @@
+#printing a number
+print(7)
+
+#printing in a new line
+print("hello world \n")
+
+#printing multiple statements
+print("hello",78)
+
+#end statement
+print("My name is Ira", end="#ira")
+print("\n")
+
+
+Ira= 13
+x= "Ira"
+print(x,"is",Ira,"years old")
+print("\n")
+
+import keyword
+print(keyword.kwlist)
