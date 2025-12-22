e53b1efcccaffce3fd6e50e471747d92e6af6d44
zarafini <ira@proedge.co>
2025-12-22 11:33:20 +0530

    Add files via upload

---
+++ Storing Birthdays Practice Project.py
@@ -0,0 +1,11 @@
+Anaya= "6th december"
+Ira= "18th february"
+Tejdeep= "2nd december"
+Jasmine= "21st october"
+Vamsi= "25th june"
+
+print("Anaya's birthday is on",Anaya)
+print("Ira's birthday is on",Ira)
+print("Tejdeep's birthday is on",Tejdeep)
+print("Jasmine's birthday is on",Jasmine)
+print("Vamsi's birthday is on",Vamsi)
