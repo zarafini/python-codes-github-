15344a2793b01653f8992c5627d87d6796e9ef78
zarafini <ira@proedge.co>
2025-12-29 20:27:02 +0530

    Add files via upload

---
+++ checkage.py
@@ -0,0 +1,6 @@
+age= int(input("Enter your age here:"))
+
+if age >  10 and age < 20:
+    print("You are eligible to attend this class.")
+else:
+    print("You are not eligible to attend this class.")
