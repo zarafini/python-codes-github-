be0a44fa69876a5828a28d4bab6fc66b1e49cb07
zarafini <ira@proedge.co>
2026-01-04 10:01:16 +0530

    Add files via upload

---
+++ mirroredtrianglepracticeproject.py
@@ -0,0 +1,14 @@
+rows= int(input("Enter the number of rows here: "))
+
+for i in range(rows - 1, 0, -1):
+    print('#'*i)
+
+
+
+
+
+rows= int(input("Enter the number of rows here: "))
+
+for i in range(1, rows + 1):
+    print('#'*i)
+    print()
