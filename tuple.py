0fdfb0a3780db1df76ffacc167b94cfda6636b9e
zarafini <ira@proedge.co>
2026-01-15 19:19:21 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,16 @@
+tuple1 = (1, 2, 3, 'boy', 'girl', True, False, 0.022)
+print(tuple1)
+
+tuple2 = (2, 4, 6)
+print(tuple2)
+
+tuple3 = tuple2 + (9, 1)
+print(tuple3)
+
+tuple4pqr = (1, 9, 2, 9, 4, 6, 6, 1, 8, 9)
+tuple4 = (tuple4pqr.count(9))
+print(tuple4)
+
+tuple5pqr = (1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+tuple5 = tuple5pqr[0:7]
+print(tuple5)
