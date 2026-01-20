78644c2ed2a759cc17e171a447c1f140519af1c4
zarafini <ira@proedge.co>
2026-01-20 16:00:31 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,8 @@
+# Write a program to create an array with the following elements - [1, 3, 5, 3, 7, 9, 3].
+# Then find the number of occurrences of number 3 in the array.
+# Also, print the reversed array.
+
+array1= [1, 3, 5, 3, 7, 9, 3]
+
+array2= array1.count(3)
+print(array2)
